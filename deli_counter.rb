5ef601b34e10b85c032eb07d5ac38b5715b7b808
William West <wwest466@gmail.com>
2020-08-20 17:19:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+katz_deli = []
+
+def line(arr)
+
+count = 0
+position = 1
+
+status = ""
+
+  if arr.length == 0
+    status = " empty."
+  else
+    status = ": " << arr[0];
+  end
+
+puts "The line is currently#{status}"
+
+end
+
+def take_a_number(arr, name)
+
+end
+
+def now_serving(arr)
+
+end
