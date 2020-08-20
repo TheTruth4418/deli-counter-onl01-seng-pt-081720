0237907a0d3f86ccdcb3bd6d95f8268c1bf796fc
William West <wwest466@gmail.com>
2020-08-20 17:36:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,23 +2,18 @@ katz_deli = []
 
 def line(arr)
 
-count = 0
-
-
-status = ""
-
-  if arr.length == 0
-    status = " empty."
-  else
-    status =  arr.each do|x|
-      arr.join("")
+  if arr >= 1
+    arr2=[]
+    count=0
+    arr.each do|x|
+        arr2.push("#{count}. #{x}")
+        count+=1
     end
-  end
-
-puts "The line is currently#{status}"
-
+    puts "The line is currently: #{arr2.join(" ")}"
+  else
+    puts "The line is currently empty."
 end
-
+end
 def take_a_number(arr, name)
 
 end
